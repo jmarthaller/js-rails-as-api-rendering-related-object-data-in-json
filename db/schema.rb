99b689ab929929065b821fe3ee2b1b4684400dad
Jacob Marthaller <https://github.com/jmarthaller>
2021-01-21 16:26:18 -0500

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_14_104321) do
-
-  create_table "birds", force: :cascade do |t|
-    t.string "name"
-    t.string "species"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
